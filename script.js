d0f430e082a4ee996d9da02b8f451bc421a85584
Hasan <hasan.myoffice2@gmail.com>
2026-09-10 14:17:26 +0600

    Coundown Section Complete

--- script.js
+++ script.js
@@ -22,3 +22,42 @@ const testimonialSwiper = new Swiper(".testimonialSwiper", {
       },
    },
 });
+
+
+// Countdown target date
+const targetDate = new Date("2026-12-31T23:59:59").getTime();
+
+const countdown = setInterval(function () {
+
+   const now = new Date().getTime();
+   const distance = targetDate - now;
+
+   // Calculate time
+   const days = Math.floor(distance / (1000 * 300 * 300 * 24));
+   const hours = Math.floor(
+      (distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+   );
+   const minutes = Math.floor(
+      (distance % (1000 * 60 * 60)) / (1000 * 60)
+   );
+   const seconds = Math.floor(
+      (distance % (1000 * 60)) / 1000
+   );
+
+   // Show result
+   document.getElementById("days").innerText = String(days).padStart(2, "0");
+   document.getElementById("hours").innerText = String(hours).padStart(2, "0");
+   document.getElementById("minutes").innerText = String(minutes).padStart(2, "0");
+   document.getElementById("seconds").innerText = String(seconds).padStart(2, "0");
+
+   // When countdown ends
+   if (distance < 0) {
+      clearInterval(countdown);
+
+      document.getElementById("days").innerText = "00";
+      document.getElementById("hours").innerText = "00";
+      document.getElementById("minutes").innerText = "00";
+      document.getElementById("seconds").innerText = "00";
+   }
+
+}, 1000);
